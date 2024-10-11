6127adba695f92f505b89f3aaab92f8f1b65438d
이윤정 <102017351+leeyunjeong010422@users.noreply.github.com>
2024-10-11 17:55:50 +0900

    feat: 사운드매니저 추가 (소리 추가X)

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager Instance;
+
+    [SerializeField] private AudioSource startBGM;
+    [SerializeField] private AudioSource gunClip;
+    [SerializeField] private AudioSource reloadClip;
+    [SerializeField] private AudioSource birdDieClip;
+    [SerializeField] private AudioSource birdMoveClip;
+    [SerializeField] private AudioSource gameOverClip;
+    [SerializeField] private AudioSource mouseClickClip;
+
+    private float previousTime = 0f;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void SetStartBGM()
+    {
+        PlayBGM(startBGM);
+    }
+
+    public void PlayBGM(AudioSource bgm)
+    {
+        bgm.loop = true;
+        bgm.Play();
+        previousTime = 0f;
+    }
+
+    public void StopBGM()
+    {
+        previousTime = startBGM.time; //startBGM의 현재 시간 저장
+        startBGM.Stop();
+    }
+
+    public void PlayGunSound()
+    {
+        gunClip.PlayOneShot(gunClip.clip);
+    }
+
+    public void PlayReloadSound()
+    {
+        reloadClip.PlayOneShot(reloadClip.clip);
+    }
+
+    public void PlayBirdDieSound()
+    {
+        birdDieClip.PlayOneShot(birdDieClip.clip);
+    }
+
+    public void PlayBirdMoveSound()
+    {
+        birdMoveClip.PlayOneShot(birdMoveClip.clip);
+    }
+
+    public void PlayGameOverSound()
+    {
+        gameOverClip.PlayOneShot(gameOverClip.clip);
+    }
+
+    public void PlayMouseClickSound()
+    {
+        mouseClickClip.PlayOneShot(mouseClickClip.clip);
+    }
+
+}
